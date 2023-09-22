8e86f80573d8373add7f6c7d900a0650d7447ee3
Michael Hooper <michaelhooper44@gmail.com>
2023-09-22 18:53:25 -0400

    Initial backbone of front-end

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import Home from './pages/Home';
-import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Golf Score Tracker</h1>
+          <nav>
+            <ul>
+              <li><Link to="/">Dashboard</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              {/* Add more navigation links here */}
+            </ul>
+          </nav>
+        </header>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <Router>
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/profile">
+            <Profile />
           </Route>
           <Route path="/">
-            <Home />
+            <Dashboard />
           </Route>
+          {/* Add more routes as needed */}
         </Switch>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
